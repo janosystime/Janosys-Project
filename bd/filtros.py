f008109b7774325b511254ad8636faad2434a84f
PedroChaim <spock_chaim@hotmail.com>
2025-09-27 15:04:38 -0300

    adicionado grafico small

--- bd/filtros.py
+++ bd/filtros.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 
 # Carregar os dados do CSV
-df = pd.DataFrame(pd.read_csv('tabelaAPIMockada.csv', encoding='latin1', sep=';'))
+df = pd.DataFrame(pd.read_csv('bd\df_api_mock.csv', encoding='latin1', sep=';'))
 
 # Filtrar as colunas relevantes
 totalRegioes_df = df[['Regiao', 'CENSO 2010', 'CENSO 2022']]
@@ -10,6 +10,6 @@ totalRegioes_df = df[['Regiao', 'CENSO 2010', 'CENSO 2022']]
 
 # Criar o gráfico de barras
 fig = px.bar(totalRegioes_df, x='Regiao', y=['CENSO 2010', 'CENSO 2022'], title='População por Região (2010 vs 2022)', 
-labels={'Diferença': 'Diferença de População', 'Regiao': 'Região'}, barmode='group')
+labels={'Diferença': 'Diferença de População', 'Regiao': 'Região'})
 
-fig.write_html('grafico_regioes.html')# Salvar o gráfico como um arquivo HTML
+fig.write_html('templates\iframes\TotalRegioesSMALL.html')

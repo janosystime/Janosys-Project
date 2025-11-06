f7231b5acdad60bd53412c64ecc02b01305b5d52
Lucas <lucasnascimento1245@gmail.com>
2025-11-06 12:21:02 -0300

    <fix> pra rodar no flask e no vercel

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-import os
 
 app = Flask(__name__)
 
@@ -11,14 +10,6 @@ def index():
 def grafico(nome):
     return render_template(f'iframes/{nome}.html')
 
-if __name__ == '__main__':
-    # Executa localmente com livereload apenas se estiver em desenvolvimento
-    if os.environ.get('FLASK_ENV') == 'development':
-        from livereload import Server
-        server = Server(app.wsgi_app)
-        server.watch('templates/*.*')
-        server.watch('static/*.*')
-        server.serve(host='0.0.0.0', port=5000, debug=True)
-    else:
-        # Modo produção padrão (para Vercel)
-        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# Exporte o app como "handler" para a Vercel
+def handler(event, context):
+    return app(event, context)

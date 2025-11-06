206937caaf6f76b3a992539bbc9f20a3fc119137
Lucas <lucasnascimento1245@gmail.com>
2025-11-06 12:12:31 -0300

    <fix> pra rodar no flask e no vercel e live server do python

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,13 @@ def grafico(nome):
     return render_template(f'iframes/{nome}.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Executa localmente com livereload apenas se estiver em desenvolvimento
+    if os.environ.get('FLASK_ENV') == 'development':
+        from livereload import Server
+        server = Server(app.wsgi_app)
+        server.watch('templates/*.*')
+        server.watch('static/*.*')
+        server.serve(host='0.0.0.0', port=5000, debug=True)
+    else:
+        # Modo produção padrão (para Vercel)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

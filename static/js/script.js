a27d244deaa20f8cc8b1a420af44add4707f502b
LuccasLukaDev <luccasluka@gmail.com>
2025-11-17 08:51:27 -0300

    feat fix: Correção e implementação de novas funcionalidades nos gráficos existentes

--- static/js/script.js
+++ static/js/script.js
@@ -9,24 +9,24 @@ const labelCard2 = document.getElementById('label-card2')
 const card3 = document.getElementById('card-idosos')
 const labelCard3 = document.getElementById('label-card3')
 
-function chamaGraphDensPop(){
-    iframe.src = './iframes/grafico_regioes.html'
-    labelCard1.textContent = 'População Total';
-    labelCard2.textContent = 'Maior População';
-    labelCard3.textContent = 'Menor População';
-}
+// function chamaGraphDensPop(){
+//     iframe.src = './iframes/grafico_regioes.html'
+//     labelCard1.textContent = 'População Total';
+//     labelCard2.textContent = 'Maior População';
+//     labelCard3.textContent = 'Menor População';
+// }
 
-function chamaGraphEtariaRegiao(){
-    iframe.src = './iframes/grafico_etaria_regiao.html'
+// function chamaGraphEtariaRegiao(){
+//     iframe.src = './iframes/grafico_etaria_regiao_2022.html'
     
-    labelCard1.textContent = 'Adultos (20-59)';
-    labelCard2.textContent = 'Região com + Crianças';
-    labelCard3.textContent = 'Região com + Idosos';
-}
+//     labelCard1.textContent = 'Adultos (20-59)';
+//     labelCard2.textContent = 'Região com + Crianças';
+//     labelCard3.textContent = 'Região com + Idosos';
+// }
 
-function chamaGraphCreche(){
-    iframe.src = './iframes/grafico_creche.html'
-    labelCard1.textContent = 'Crianças em Creche';
-    labelCard2.textContent = '% Cobertura Creche';
-    labelCard3.textContent = 'Vagas Disponíveis';
-}
+// function chamaGraphCreche(){
+//     iframe.src = './iframes/grafico_creche.html'
+//     labelCard1.textContent = 'Crianças em Creche';
+//     labelCard2.textContent = '% Cobertura Creche';
+//     labelCard3.textContent = 'Vagas Disponíveis';
+// }

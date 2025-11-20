9b5d6f45eb99c08c1b2b79d38035d59d0993bab2
PedroChaim <spock_chaim@hotmail.com>
2025-11-19 23:58:31 -0300

    feat: grafico html de renda criado

--- public/bd/filtro_renda.py
+++ public/bd/filtro_renda.py
@@ -4,9 +4,8 @@ import plotly.express as px
 ## Renda Media 2010
 ## Renda media em 2010 = 3669.52
 df_renda_pc_2010 = pd.read_csv('public/bd/df_renda_media_regiao.csv', sep=',')
-df_renda_pc_2010["Desvio %"] = (df_renda_pc_2010['renda_media_2010'] / 3669.52).round(2)
-df_renda_pc_2010["Diferença %"] = (df_renda_pc_2010['Desvio %'] -1).round(2)
-
+df_renda_pc_2010["Desvio %"] = (df_renda_pc_2010['renda_media_2010'] / 3669.52)
+df_renda_pc_2010["Diferença %"] = ((df_renda_pc_2010['Desvio %'] -1)*100).apply(lambda x: f"{x:.2f}%")
 df_renda = df_renda_pc_2010
 
 ## Renda Media 2025
@@ -22,7 +21,7 @@ df_renda
 
 
 ## Printar Grafico
-fig = px.line(df_renda, x='regiao', y=['Desvio %', 'renda_media_2010'], text='Desvio %')
+fig = px.line(df_renda, x='regiao', y=['Diferença %', 'renda_media_2010'], text='Diferença %')
 fig.add_shape(
 type="line",
 x0=df_renda['regiao'].iloc[0],   # início da linha em x
@@ -39,7 +38,8 @@ font=dict(color="red", size=12),
 xanchor="left",
 yanchor="bottom"
 )
-fig.update_layout(xaxis_title=" ",yaxis_title="Diferença em relação ao rendimento médio.", title_x=0.5, plot_bgcolor="white", bargap=0.2, showlegend=False)
+fig.update_layout(xaxis_title=" ",yaxis_title="Rendimento Médio (2010)", title_x=0.5, plot_bgcolor="white", bargap=0.2, showlegend=False)
 fig.update_traces(textposition='top right')
 fig.show()
+fig.write_html('public/bd/grafico_renda.html', full_html=True, include_plotlyjs="cdn")
 
